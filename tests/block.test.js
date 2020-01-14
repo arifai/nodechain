52846799705c55d2b0d6bdb5c7617b7b0999c20b
Ahmad Rifa'i <arifai209@gmail.com>
2020-01-14 13:50:21 +0700

    create genesis block

---
+++ tests/block.test.js
@@ -0,0 +1,51 @@
+const Block = require('../block');
+const { GENESIS_DATA } = require('../config');
+
+describe('Block', () => {
+    const timestamp = '2020-20-20';
+    const lastHash = 'foo-lastHash';
+    const hash = 'foo-hash';
+    const data = ['foo-data', 'block-data', 'foo-block'];
+    const block = new Block({ timestamp, lastHash, hash, data });
+
+    it('has a timestamp, lastHash, hash, data property', () => {
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+    });
+
+    describe('genesis()', () => {
+        const genesisBlock = Block.genesis();
+
+        it('returns a Block instance', () => {
+            expect(genesisBlock instanceof Block).toBe(true);
+        });
+
+        it('returns a genesis data', () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        });
+    });
+
+    describe('mineBlock()', () => {
+        const lastBlock = Block.genesis();
+        const data = 'mined data';
+        const minedBlock = Block.mineBlock({ lastBlock, data });
+
+        it('returns a Block instance', () => {
+            expect(minedBlock instanceof Block).toBe(true);
+        });
+
+        it('sets the `lastHash` to be the `hash` of lastBlock', () => {
+            expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+        });
+
+        it('sets the `data`', () => {
+            expect(minedBlock.data).toEqual(data);
+        });
+
+        it('sets a `timestamp`', () => {
+            expect(minedBlock.timestamp).not.toEqual(undefined);
+        });
+    });
+});
