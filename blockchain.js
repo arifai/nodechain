b5c1c90320729958c30311ce494a594bf99e1bff
Ahmad Rifa'i <arifai209@gmail.com>
2020-01-14 16:21:30 +0700

    stub console output in tests

--- blockchain.js
+++ blockchain.js
@@ -17,13 +17,16 @@ class Blockchain {
 
     replaceChain(chain) {
         if (chain.length <= this.chain.length) {
+            console.error('The incoming chain must be longer');
             return;
         }
 
         if (!Blockchain.isValidChain(chain)) {
+            console.error('The incoming chain must be vallid')
             return;
         }
 
+        console.log('replacing chain with', chain);
         this.chain = chain;
     }
 
