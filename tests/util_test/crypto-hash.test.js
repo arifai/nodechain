69bfb2352559ccf64bb30b848985521fbd587e86
Ahmad Rifa'i <arifai209@gmail.com>
2020-01-15 16:38:08 +0700

    fixup crypto-hash

--- tests/util_test/crypto-hash.test.js
+++ tests/util_test/crypto-hash.test.js
@@ -2,11 +2,20 @@ const cryptoHash = require('../../util/cryto-hash');
 
 describe('cryptoHash()', () => {
     it('generates a SHA-256 hashed output', () => {
-        const sha256 = '9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08'.toLowerCase();
+        const sha256 = '4D967A30111BF29F0EBA01C448B375C1629B2FED01CDFCC3AED91F1B57D5DD5E'.toLowerCase();
         expect(cryptoHash('test')).toEqual(sha256);
     });
 
     it('produces the same hash with the same input arguments in any order', () => {
-        expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('three', 'two', 'one'));
+        expect(cryptoHash('one', 'two', 'three'))
+            .toEqual(cryptoHash('three', 'two', 'one'));
+    });
+
+    it('produces a unique hash when the properties have changed on an input', () => {
+        const foo = {};
+        const originalHash = cryptoHash(foo);
+        foo['a'] = 'a';
+
+        expect(cryptoHash(foo)).not.toEqual(originalHash);
     });
 });
