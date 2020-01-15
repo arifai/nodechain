b20a4aa3d98c9cd6df855c73df284aa5041fe393
Ahmad Rifa'i <arifai209@gmail.com>
2020-01-15 17:13:15 +0700

    create transaction pool & set transactions

---
+++ tests/wallet_test/transaction-pool.test.js
@@ -0,0 +1,25 @@
+const TransactionPool = require('../../wallet/transaction-pool');
+const Transaction = require('../../wallet/transaction');
+const Wallet = require('../../wallet/index');
+
+describe('TransactionPool', () => {
+    let transactionPool, transaction;
+
+    beforeEach(() => {
+        transactionPool = new TransactionPool();
+        transaction = new Transaction({
+            senderWallet: new Wallet(),
+            recipient: 'dummy-recipient',
+            amount: 50
+        });
+    });
+
+    describe('setTransaction()', () => {
+        it('adds a transaction', () => {
+            transactionPool.setTransaction(transaction);
+
+            expect(transactionPool.transactionMap[transaction.id])
+                .toBe(transaction);
+        });
+    });
+});
